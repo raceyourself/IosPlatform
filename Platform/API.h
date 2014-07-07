c2694c249b377c204ba233979f0a2f88a5c78010
Janne Husberg <janne@Bens-MacBook-Air.local>
2014-07-07 16:38:50 +0100

    Refactored login code into API class method

---
+++ Platform/API.h
@@ -0,0 +1,13 @@
+//
+//  API.h
+//  Platform
+//
+//  Created by Janne Husberg on 07/07/2014.
+//  Copyright (c) 2014 raceyourself. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface API : NSObject
+
+@end
---
+++ Platform/API.m
@@ -0,0 +1,13 @@
+//
+//  API.m
+//  Platform
+//
+//  Created by Janne Husberg on 07/07/2014.
+//  Copyright (c) 2014 raceyourself. All rights reserved.
+//
+
+#import "API.h"
+
+@implementation API
+
+@end
